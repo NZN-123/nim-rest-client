44141abac19969dc7e028c75b798c0cf35aaae5b
NZN-123 <jngjn02@gmail.com>
2026-07-08 15:39:13 +0900

    feat: 학습 계획 생성 폼 제출 처리 및 NIM API 연동 기초 구현

---
+++ src/main/java/org/example/nimrestclient/util/NimUtil.java
@@ -0,0 +1,24 @@
+package org.example.nimrestclient.util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+public class NimUtil {
+    private final String nimApiKey = System.getenv("NIM_API_KEY");
+    private final String nimApiURL = "invoke_url='https://integrate.api.nvidia.com/v1/chat/completions'\n";
+    private final RestClient restClient;
+
+    protected NimUtil() {
+        System.out.println("nimApiKey = " + nimApiKey.substring(0, 10) + "...");
+        restClient = RestClient.builder()
+                .baseUrl(nimApiURL)
+                .defaultHeader("Authorization", "Bearer %s".formatted(nimApiKey))
+                .defaultHeader("Content-Type", "application/json")
+                .build();
+    }
+
+    public String useNim(String prompt) {
+        return "%s라니 멋지군요!".formatted(prompt);
+    }
+}
--- src/main/webapp/WEB-INF/views/plan.jsp
+++ src/main/webapp/WEB-INF/views/plan.jsp
@@ -7,6 +7,15 @@
     <title>학습 계획을 짜드립니다.</title>
 </head>
 <body>
-    <h1>학습 계획 짜드림</h1>
+<h1>학습 계획 짜드림</h1>
+<form method="post">
+    <input name="subject" placeholder="과목을 입력하세요">
+    <button>계획짜기</button>
+</form>
+<section>
+    <p>
+        <c:out value="${plan}"></c:out>
+    </p>
+</section>
 </body>
 </html>
