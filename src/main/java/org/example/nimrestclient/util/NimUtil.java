72cf80235d0a5269eab09a71e6937328b1184f99
NZN-123 <jngjn02@gmail.com>
2026-07-08 15:59:12 +0900

    fix: NimUtil의 API URL 특수문자 오류 수정 및 DTO 연동 보완

--- src/main/java/org/example/nimrestclient/util/NimUtil.java
+++ src/main/java/org/example/nimrestclient/util/NimUtil.java
@@ -1,16 +1,22 @@
 package org.example.nimrestclient.util;
 
+import org.example.nimrestclient.dto.NimRequestDTO;
+import org.example.nimrestclient.dto.NimResponseDTO;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
 @Component
 public class NimUtil {
     private final String nimApiKey = System.getenv("NIM_API_KEY");
-    private final String nimApiURL = "invoke_url='https://integrate.api.nvidia.com/v1/chat/completions'\n";
+    private final String nimApiURL = "https://integrate.api.nvidia.com/v1/chat/completions";
     private final RestClient restClient;
 
     protected NimUtil() {
-        System.out.println("nimApiKey = " + nimApiKey.substring(0, 10) + "...");
+        if (nimApiKey != null && nimApiKey.length() >= 10) {
+            System.out.println("nimApiKey = " + nimApiKey.substring(0, 10) + "...");
+        } else {
+            System.out.println("nimApiKey is null or too short!");
+        }
         restClient = RestClient.builder()
                 .baseUrl(nimApiURL)
                 .defaultHeader("Authorization", "Bearer %s".formatted(nimApiKey))
@@ -19,6 +25,18 @@ public class NimUtil {
     }
 
     public String useNim(String prompt) {
-        return "%s라니 멋지군요!".formatted(prompt);
+//        String model = "z-ai/glm-5.2";
+        String model = "deepseek-ai/deepseek-v4-flash";
+//        String response = restClient.post() //
+        NimResponseDTO response = restClient.post() //
+                .body(NimRequestDTO.of(model, prompt))
+                .retrieve()
+                // https://jsonformatter.org/
+                // String -> DTO
+//                .body(String.class);
+                .body(NimResponseDTO.class);
+        System.out.println("response = " + response);
+//        return "%s라니 멋지군요!".formatted(prompt);
+        return response.getResult();
     }
 }
